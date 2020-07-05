7a962c5bbb2e4d84d682f671e9369d30b2f5e468
imdeepakmishra <deepakmishra3813@gmail.con>
2020-07-05 11:54:45 +0530

    React-App

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+const header = () => {
+  return (
+    <header className="center">
+      <img src={logo} alt="" />
+    </header>
+  );
+};
+
+export default header;
